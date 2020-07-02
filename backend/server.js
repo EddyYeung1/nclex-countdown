50b45415b0c0a32c1f744c58c8bd9a095c3b1844
Eddy Yeung <yeung.edward36@gmail.com>
2020-07-02 17:28:25 -0400

    trying another thing that won't work

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use("/prizes", prizeRouter);
 app.use("/users", usersRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../build"));
+  app.use(express.static(path.resolve(__dirname, "..", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
